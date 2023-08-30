9418b6712d8b0ddc4b19ab5a94806a6293e8979b
dimon-hacker <dimon-hacker@yandex.ru>
2023-08-30 20:22:37 +0300

    main section

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -4,12 +4,31 @@ import {ProjectItem} from "./projectItem/ProjectItem";
 import {ColorSpan} from "../../../components/ColorSpan";
 import {SectionTitle} from "../../../components/SectionTitle";
 import ChertProj from '../../../../src/assets/images/Rectangle 22.png'
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
 export const Projects = () => {
     return (
         <StyledSProjects>
+
             <SectionTitle><ColorSpan>#</ColorSpan>projects</SectionTitle>
-            <ProjectItem imgSrc={ChertProj} skills={"HTML CSS Python Flask"} title={"ChertNodes"} description={"Minecraft servers hosting "} live={"live<~>"} cashed={""}></ProjectItem>
+            <FlexWrapper justify={"space-between"} align={"start"}>
+                <StyledDiv>
+                    <ProjectItem imgSrc={ChertProj} skills={"HTML CSS Python Flask"} title={"ChertNodes"}
+                                 description={"Minecraft servers hosting "} live={"live<~>"} cashed={""}></ProjectItem>
+                </StyledDiv>
+                <StyledDiv>
+                    <ProjectItem imgSrc={ChertProj} skills={"HTML CSS Python Flask"} title={"ChertNodes"}
+                                 description={"Minecraft servers hosting "} live={"live<~>"} cashed={""}></ProjectItem>
+                </StyledDiv>
+                <StyledDiv>
+                    <ProjectItem imgSrc={ChertProj} skills={"HTML CSS Python Flask"} title={"ChertNodes"}
+                                 description={"Minecraft servers hosting "} live={"live<~>"} cashed={""}></ProjectItem>
+                </StyledDiv>
+                <StyledDiv>
+                    <ProjectItem imgSrc={ChertProj} skills={"HTML CSS Python Flask"} title={"ChertNodes"}
+                                 description={"Minecraft servers hosting "} live={"live<~>"} cashed={""}></ProjectItem>
+                </StyledDiv>
+            </FlexWrapper>
         </StyledSProjects>
     );
 };
@@ -17,4 +36,9 @@ const StyledSProjects = styled.section`
   background-color: #606060;
   min-height: 100vh;
   border: white;
+  padding: 20px;
+  position: relative;
+`
+const StyledDiv = styled.div`
+  margin: 20px;
 `
